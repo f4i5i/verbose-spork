786c6def50f3faffa2a8289030bcabf50b694553
rocket1337 <rocket1337@localhost.localdomain>
2020-02-21 23:35:26 +0500

    database dj-django-url

--- ittfApp/settings.py
+++ ittfApp/settings.py
@@ -168,3 +168,6 @@ RQ_QUEUES = {
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
+import dj_database_url
+prod_db = dj_database_url.config(conn_max_age=3000)
+DATABASES['default'].update(prod_db)
