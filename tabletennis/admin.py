872dbada83bce40913babe0f98ed876de86870be
rocket1337 <rocket1337@localhost.localdomain>
2020-03-11 19:37:57 +0500

    Added Filters and serach field to Comp and Phases

--- tabletennis/admin.py
+++ tabletennis/admin.py
@@ -1,9 +1,22 @@
 from django.contrib import admin
+from django_admin_listfilter_dropdown.filters import (
+    DropdownFilter, ChoiceDropdownFilter, RelatedDropdownFilter
+)
 
 # Register your models here.
 from .models import *
 
-admin.site.register(Competition)
+class PhasesAdmin(admin.ModelAdmin):
+    list_display = ('key','desc','evkey','phase_type')
+    list_filter = (('phase_type',DropdownFilter),
+                    ('key',DropdownFilter),)
+
+class CompAdmin(admin.ModelAdmin):
+    list_display = ('champ','description','location', 'url')
+    list_filter = (('location',DropdownFilter),)
+    search_fields = ('champ','location','description')
+
+admin.site.register(Competition,CompAdmin)
 admin.site.register(RawData)
-admin.site.register(Phases)
+admin.site.register(Phases,PhasesAdmin)
 admin.site.register(Table)
