9ade6ad1a116cca946a591b0be36d32325439b39
rocket1337 <rocket1337@localhost.localdomain>
2020-03-13 00:41:59 +0500

    added new three new views

--- tabletennis/urls.py
+++ tabletennis/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path,include
-from .views import view1,view2
+from .views import view1,view2,view3,view4
 
 urlpatterns = [
     path('',view1,name="view1"),
     path('v2',view2,name="view2"),
-
+    path('v3',view3,name="view3"),
+    path('v4',view4,name="view4"),
 ]
--- tabletennis/views.py
+++ tabletennis/views.py
@@ -10,11 +10,20 @@ from .utils2 import get_match_urls,get_country,get_player_data
 def view1(request):
     queue = django_rq.get_queue('default',is_async=True,default_timeout=30000)
     queue.enqueue(scrape)
-    return HttpResponse("Scraping..........")
+    return HttpResponse("Scraping Competitions..........")
 
 def view2(request):
     queue = django_rq.get_queue('default',is_async=True,default_timeout=30000)
-    queue.enqueue(get_player_data)
-    return HttpResponse("Scraping..........")
-   
+    queue.enqueue(get_match_urls)
+    return HttpResponse("Scraping Match Urls..........")
 
+def view3(request):
+    queue = django_rq.get_queue('default',is_async=True,default_timeout=30000)
+    queue.enqueue(get_country)
+    return HttpResponse("Scraping Country..........")
+
+
+def view4(request):
+    queue = django_rq.get_queue('default',is_async=True,default_timeout=30000)
+    queue.enqueue(get_player_data)
+    return HttpResponse("Scraping Players..........")
